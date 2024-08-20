b90123dbc04acdfadb35135bcd326c88077d0af1
gowtham <gowtham6008@gmail.com>
2024-08-20 17:10:17 +0530

    url changed

--- src/main/java/com/project/inventorymanagementsystem/config/WebConfig.java
+++ src/main/java/com/project/inventorymanagementsystem/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**").allowedOrigins("https://inventory-frontend-pcs7ow18h-gowthams-projects-cd0d01f0.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
